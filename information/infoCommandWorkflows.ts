d601c1bd0630df1eb9d4bc2d0ae5924305c67c99
RocketCat <dakondo@protonmail.com>
2021-03-09 12:58:31 +0100

    moved files

--- information/infoCommandWorkflows.ts
+++ information/infoCommandWorkflows.ts
@@ -4,14 +4,12 @@ import {
 } from "ts-results"
 import {Document} from "mongoose"
 
-import {InfoCommand} from "./types"
-import {InfoCommandModel} from "./models"
+import {InfoCommand} from "./InfoCommand"
 
-export async function addInfoCommandWorkflow(channel:string,cmd:string,info:string,throttle?:number)
+export async function createInfoCommandWorkflow(channel:string,cmd:string,info:string,throttle?:number)
 :Promise<Result<void,Error>>{
     try{
         //TODO check if cmd exists 
-
         const infoCmd=InfoCommand.create(channel,cmd,info,throttle)
             .expect("creating info command");
 
@@ -22,27 +20,29 @@ export async function addInfoCommandWorkflow(channel:string,cmd:string,info:stri
 }
 
 export async function deleteInfoCommandWorkflow(channel:string,cmd:string):Promise<Result<void,Error>>{
-    const res=await InfoCommandModel.deleteOne({channel,cmd})
-    if(!res.ok){
-        return Err(new Error("not deleted"))
-    }
-    return Ok.EMPTY
+	try{
+		(await InfoCommand.delete(channel,cmd)).expect("deleting info command")
+		return Ok.EMPTY
+	}catch(e){ return Err(e) }
 }
 
 export async function updateInfoCommandWorkflow(channel:string,cmd:string,info?:string,throttle?:number)
 :Promise<Result<void,Error>>{
-    const res=await InfoCommandModel.updateOne({channel,cmd},{channel,cmd,info,throttle})
-    if(!res.ok){
-        return Err(new Error("not updated"))
-    }
+	const infoCmd=(await InfoCommand.get(channel,cmd)).expect("no such channel command");
+
+    (await infoCmd.update(cmd,info,throttle))
+	.expect("updating info command");
     return Ok.EMPTY
 }
 
 export async function getInfoCommandWorkflow(channel:string,cmd:string)
 :Promise<Result<InfoCommand,Error>>{
-    const doc:Document<InfoCommand>=await InfoCommandModel.findOne({channel,cmd})
-    if(!doc){
-        return Err(new Error("not found"))
-    }
-    return Ok(doc)
+	try{
+		console.log("channel:", channel)
+		console.log("CMD:", cmd)
+		const infoCmd=(await InfoCommand.get(channel,cmd))
+		.expect("get info command");
+
+		return Ok(infoCmd)
+	}catch(e){return Err(e)}
 }
